9fddd6a13f95af65786d3b4d7f8224851824d7ea
igor <igrokyse@gmail.com>
2022-05-21 22:46:58 -0700

    adding dist subtree

---
+++ dist/js/about.e527b65a.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkresume"]=self["webpackChunkresume"]||[]).push([[443],{7381:function(e,n,u){u.r(n),u.d(n,{default:function(){return l}});var r=u(3396);const s={class:"about"},t=(0,r._)("h1",null,"This is an about page",-1),a=[t];function c(e,n){return(0,r.wg)(),(0,r.iD)("div",s,a)}var i=u(89);const o={},f=(0,i.Z)(o,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=about.e527b65a.js.map
