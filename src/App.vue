664fdf7461742bc2f89cd16ea8588e5da5de4b71
Dargonit <igrokyse@gmail.com>
2026-02-09 20:30:15 -0800

    replaced default stuff added welcome message added links component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template> 
+  <HelloWorld id="hello-world" msg="Welcome Stranger!"/>
+  <NavLinks></NavLinks>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import NavLinks from './components/Links.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    NavLinks
   }
 }
+
+
 </script>
 
 <style>
+body {
+  margin:0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #dfe4ea;
+  background-color: #2e2e2e;
+  height:100vh;
+  padding:0%;
+    margin: 0%;
+}
+#hello-world {
+  padding-top:45%;
+  
 }
 </style>
---
+++ src/components/Links.vue
@@ -0,0 +1,22 @@
+<template >
+    <div id="container">
+    <h1>Link1</h1><h2>Link2</h2>
+    <H1>Link 3</H1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'NavLinks',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    #container{
+        display:block;
+    }
+</style>
